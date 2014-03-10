08b6ab470d587b411bd47c1f2514d29ffb77a41c
Vile <ville.herranen@gmail.com>
2014-03-10 19:26:06 +0200

    Added a rude laser implementation.

--- laser/src/com/flimpure/laser/entity/Player.java
+++ laser/src/com/flimpure/laser/entity/Player.java
@@ -1,12 +1,16 @@
 package com.flimpure.laser.entity;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.flimpure.laser.assets.Assets;
 
 public class Player extends Entity {
 
+	public Vector2 laserTarget;
+
     public Player(float x, float y, float width, float height) {
         super(x, y, width, height);
+        laserTarget = new Vector2(0.0f, 0.0f);
     }
 
     @Override
@@ -14,5 +18,13 @@ public class Player extends Entity {
         batch.draw(Assets.getFullGameObject("lense_shadow"), x + 0.1f, y - 0.5f, 0.8f, 0.3f);
         batch.draw(Assets.getFullGameObject("lense"), x, y, width, height);
         batch.draw(Assets.getFullGameObject("lense_hue"), x - 1f, y - 1f, 3f, 3f);
+    }
+
+    public float x() {
+    	return x;
+    }
+
+    public float y() {
+    	return y;
     }
 }
--- laser/src/com/flimpure/laser/screen/GameScreen.java
+++ laser/src/com/flimpure/laser/screen/GameScreen.java
@@ -2,6 +2,11 @@ package com.flimpure.laser.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.flimpure.laser.LaserGame;
 import com.flimpure.laser.assets.Assets;
 import com.flimpure.laser.entity.Direction;
@@ -10,69 +15,111 @@ import com.flimpure.laser.entity.Player;
 
 public class GameScreen extends Basic2DScreen {
 
-    private final ScreenShake screenShake;
-    private final Player player;
+	private final ScreenShake screenShake;
+	private final Player player;
 
-    public GameScreen(LaserGame game) {
-        super(game, 24, 16);
-        camera.position.set(12f, 8f, 0f);
-        camera.update();
-        screenShake = new ScreenShake(camera);
-        player = new Player(8f,8f,1f,1f);
-    }
+	private ShapeRenderer sr;
 
-    @Override
-    protected void updateScreen(float fixedStep) {
-        processKeys();
+	public GameScreen(LaserGame game) {
+		super(game, 24, 16);
+		camera.position.set(12f, 8f, 0f);
+		camera.update();
+		screenShake = new ScreenShake(camera);
+		player = new Player(8f, 8f, 1f, 1f);
 
-        player.update(fixedStep);
+		sr = new ShapeRenderer();
+	}
 
-        screenShake.update(fixedStep);
-    }
+	@Override
+	public void dispose() {
+		super.dispose();
+		sr.dispose();
+	}
 
-    private void processKeys() {
-        {
-            if (!paused && player.isAlive()) {
-                // process player movement keys
-                if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-                    player.moveWithAccel(Direction.UP, Entity.ACCEL_MAX);
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-                    player.moveWithAccel(Direction.DOWN, Entity.ACCEL_MAX);
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-                    player.moveWithAccel(Direction.RIGHT, Entity.ACCEL_MAX);
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-                    player.moveWithAccel(Direction.LEFT, Entity.ACCEL_MAX);
-                }
+	@Override
+	protected void updateScreen(float fixedStep) {
+		processKeys();
 
-                if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
-                    camera.zoom += 0.1f;
-                    camera.update();
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.PLUS)) {
-                    camera.zoom -= 0.1f;
-                    camera.update();
-                }
-            }
-        }
-    }
+		player.update(fixedStep);
 
-    @Override
-    public void renderScreen(float delta) {
-        spriteBatch.setProjectionMatrix(camera.combined);
-        spriteBatch.begin();
+		screenShake.update(fixedStep);
+	}
 
-        spriteBatch.draw(Assets.getFullGameObject("background-temporary"), 0, 0, 24, 16);
+	private void processKeys() {
+		{
+			if (!paused && player.isAlive()) {
+				// process player movement keys
+				if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+					player.moveWithAccel(Direction.UP, Entity.ACCEL_MAX);
+				}
+				if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+					player.moveWithAccel(Direction.DOWN, Entity.ACCEL_MAX);
+				}
+				if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+					player.moveWithAccel(Direction.RIGHT, Entity.ACCEL_MAX);
+				}
+				if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+					player.moveWithAccel(Direction.LEFT, Entity.ACCEL_MAX);
+				}
 
-        player.render(delta, spriteBatch);
-        spriteBatch.end();
-    }
+				if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
+					camera.zoom += 0.1f;
+					camera.update();
+				}
+				if (Gdx.input.isKeyPressed(Input.Keys.PLUS)) {
+					camera.zoom -= 0.1f;
+					camera.update();
+				}
+				
 
-    @Override
-    public void show() {
-        super.show();
-        Gdx.input.setInputProcessor(this);
-    }
+				if (Gdx.input.isButtonPressed(0)) {
+					// intersect laser with game objects
+					// update the laser target
+					Vector3 v2 = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0.0f);
+					camera.unproject(v2);
+
+					player.laserTarget.x = v2.x;
+					player.laserTarget.y = v2.y;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void renderScreen(float delta) {
+		spriteBatch.setProjectionMatrix(camera.combined);
+		spriteBatch.begin();
+
+		spriteBatch.draw(Assets.getFullGameObject("background-temporary"), 0,
+				0, 24, 16);
+
+		spriteBatch.end();
+
+		// shoot some laser!
+		if (Gdx.input.isButtonPressed(0)) {
+			Vector2 v1 = new Vector2(player.x() + 0.5f, player.y() + 0.5f);
+			Vector2 v2 = player.laserTarget;
+			
+			// check if the beam hits anything
+			sr.setProjectionMatrix(camera.combined);
+			sr.setColor(Color.RED);
+			sr.begin(ShapeType.Filled);
+			sr.rectLine(v1, v2, 0.25f);
+			sr.rectLine(v2, Vector2.Zero, 0.25f);
+
+			sr.end();
+		}
+		
+
+		spriteBatch.begin();
+		player.render(delta, spriteBatch);
+		spriteBatch.end();
+
+	}
+
+	@Override
+	public void show() {
+		super.show();
+		Gdx.input.setInputProcessor(this);
+	}
 }
